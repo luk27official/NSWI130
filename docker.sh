adf1f0a1763a99ae6657a9eab99852d285479328
Lukas Polak <1692163+luk27official@users.noreply.github.com>
2022-11-30 11:15:53 +0100

    Update docker.sh for Windows

--- docker.sh
+++ docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -it --rm -p 8080:8080 -v $PWD/structurizr:/usr/local/structurizr structurizr/lite
+docker run -it --rm -p 8080:8080 -v ${PWD}/structurizr:/usr/local/structurizr structurizr/lite
